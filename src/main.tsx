2af88444a817a455e17b1baaaf10e89153189a59
vahidGhadiri <whydrf@gmail.com>
2025-03-05 19:12:47 +0330

    chore(main.tsx): Wrap whole the project with query-client-provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
+import queryClient from './config/react-query';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <>
+    <QueryClientProvider client={queryClient}>
         INITIAL
-    </>
+    </QueryClientProvider>,
 );
+
+
