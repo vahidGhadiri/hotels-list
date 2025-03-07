aeda6c2ac4996826f36ea37304f89d6191d67c9c
vahidGhadiri <whydrf@gmail.com>
2025-03-08 01:47:32 +0330

    12312

--- src/presentations/hotel/router.tsx
+++ src/presentations/hotel/router.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import HotelDetail from "./detail";
-import HotelsList from "./list";
+import { Suspense, lazy } from "react";
+import { SpinnerLoading } from "@components";
+
+const HotelsList = lazy(() => import("./list"));
+const HotelDetail = lazy(() => import("./detail"));
 
 const HotelRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HotelsList />} />
-      <Route path="/hotels/:id" element={<HotelDetail />} />
-    </Routes>
+    <Suspense fallback={<SpinnerLoading mode="overlay" size="large" />}>
+      <Routes>
+        <Route path="/" element={<HotelsList />} />
+        <Route path="/hotels/:id" element={<HotelDetail />} />
+      </Routes>
+    </Suspense>
   );
 };
 
